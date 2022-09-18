0572797a50ff5399400f8f27a7972d4918ca593c
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-09-17 21:49:26 -0300

    Introducao ao Java Script

---
+++ primeiro_programa/meuprimeirocodigo.js
@@ -0,0 +1,9 @@
+//alert("Minha primeira pagina usando o java script");
+///alert(2 + 3);
+var a = 12;
+var b = 3;
+var c = a + b;
+alert("Total da soma:  " + c);
+var nome;
+nome = prompt("Qual o seu nome: ");
+alert("Olá, " + nome);
